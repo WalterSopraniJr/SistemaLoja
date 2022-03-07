6b6581b96754f35628308138b9108416c9563f42
Anna Moura Sobral <annamourasobral@gmail.com>
2022-03-07 18:16:14 +0100

    adicionando funções da loja ao código

--- produtos.js
+++ produtos.js
@@ -1,63 +1,63 @@
 const produto = [
     {
         nome: "Granola Tradicional 250g",
-        codigo: "",
+        codigo: "1",
         precoCompra: 2.50,
         precoVenda: 7.00,
         
     }, {
         nome: "Granola Tradicional 500g",
-        codigo: "",
+        codigo: "2",
         precoCompra: 4.50,
         precoVenda: 12.00,
     }, {
         nome: "Granola Tradicional 1Kg",
-        codigo: "",
+        codigo: "3",
         precoCompra: 8.00,
         precoVenda: 15.50,
     },{
         nome: "Granola Diet 250g",
-        codigo: "",
+        codigo: "4",
         precoCompra: 3.50,
         precoVenda: 8.00,
     }, {
         nome: "Granola Diet 500g",
-        codigo: "",
+        codigo: "5",
         precoCompra: 5.50,
         precoVenda: 13.00,
     }, {
         nome: "Granola Diet 1Kg",
-        codigo: "",
+        codigo: "6",
         precoCompra: 9.00,
         precoVenda: 16.50,
     }, {
         nome: "Granola Cacau 250g",
-        codigo: "",
+        codigo: "7",
         precoCompra: 2.50,
         precoVenda: 7.00,
     }, {
         nome: "Granola Cacau 500g",
-        codigo: "",
+        codigo: "8",
         precoCompra: 4.50,
         precoVenda: 12.00,
     }, {
         nome: "Granola Cacau 1Kg",
-        codigo: "",
+        codigo: "9",
         precoCompra: 8.00,
         precoVenda: 15.50,
     }, {
         nome: "Granola Caju 250g",
-        codigo: "",
+        codigo: "10",
         precoCompra: 2.50,
         precoVenda: 7.00,
     }, {
         nome: "Granola Caju 500g",
-        codigo: "",
+        codigo: "11",
         precoCompra: 4.50,
         precoVenda: 12.00,
     }, {
         nome: "Granola Caju 1Kg",
-        codigo: "",
+        codigo: "12",
         precoCompra: 8.00,
         precoVenda: 15.50,
     }
