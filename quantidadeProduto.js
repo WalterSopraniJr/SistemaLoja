9092ae3cef978d5e9f015d0582843a091f07e75e
Anna Moura Sobral <annamourasobral@gmail.com>
2022-03-07 21:27:06 +0100

    changing variables in quantity js archive

--- quantidadeProduto.js
+++ quantidadeProduto.js
@@ -1,5 +1,5 @@
-let sumar = document.getElementById("mas");
-let restar = document.getElementById("menos");
+let somar = document.getElementById("mais");
+let diminuir = document.getElementById("menos");
 let contador = document.getElementById("contador");
 let prevValue;
 
@@ -9,31 +9,34 @@ function calcular() {
   var isValid = /^[1-9][0-9]*$/.test(value);
 
   if (!isValid) {
-          contador.value = prevValue;
-        } else {
-          prevValue = value;
-        }
-}
+    contador.value = prevValue;
+    } else {
+      prevValue = value;
+    }
 
-      sumar.onclick = function() {
-        contador.value = Number(contador.value) + 1;
-        calcular();
-      };
 
-      restar.onclick = function() {
-        contador.value = Number(contador.value) - 1;
-        calcular();
-      };
-
-      contador.onchange = function() {
-        calcular();
-      };
-
-      contador.onkeyup = function() {
-        if (contador.value === "") {
-          return;
-        }
-        calcular();
-      };
-
+    somar.onclick = function() {
+      contador.value = Number(contador.value) + 1;
       calcular();
+    };
+
+    diminuir.onclick = function() {
+      contador.value = Number(contador.value) - 1;
+      calcular();
+    };
+
+    contador.onchange = function() {
+      calcular();
+    };
+
+    contador.onkeyup = function() {
+      if (contador.value === "") {
+        return;
+      }
+      calcular();
+    };
+
+    calcular();
+  
+  }
+}
