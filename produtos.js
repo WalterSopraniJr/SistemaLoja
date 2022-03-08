a6ed01a13b6b116719d6faaa4a0dbc7c4facd393
Walter Soprani Junior <waltersopranijr@gmail.com>
2022-03-08 05:19:39 -0300

    Added stock variable.

--- produtos.js
+++ produtos.js
@@ -4,62 +4,73 @@ const produto = [
         codigo: "1",
         precoCompra: 2.50,
         precoVenda: 7.00,
-        
+        estoque: 0
     }, {
         nome: "Granola Tradicional 500g",
         codigo: "2",
         precoCompra: 4.50,
         precoVenda: 12.00,
+        estoque: 0
     }, {
         nome: "Granola Tradicional 1Kg",
         codigo: "3",
         precoCompra: 8.00,
         precoVenda: 15.50,
+        estoque: 0
     },{
         nome: "Granola Diet 250g",
         codigo: "4",
         precoCompra: 3.50,
         precoVenda: 8.00,
+        estoque: 0
     }, {
         nome: "Granola Diet 500g",
         codigo: "5",
         precoCompra: 5.50,
         precoVenda: 13.00,
+        estoque: 0
     }, {
         nome: "Granola Diet 1Kg",
         codigo: "6",
         precoCompra: 9.00,
         precoVenda: 16.50,
+        estoque: 0
     }, {
         nome: "Granola Cacau 250g",
         codigo: "7",
         precoCompra: 2.50,
         precoVenda: 7.00,
+        estoque: 0
     }, {
         nome: "Granola Cacau 500g",
         codigo: "8",
         precoCompra: 4.50,
         precoVenda: 12.00,
+        estoque: 0
     }, {
         nome: "Granola Cacau 1Kg",
         codigo: "9",
         precoCompra: 8.00,
         precoVenda: 15.50,
+        estoque: 0
     }, {
         nome: "Granola Caju 250g",
         codigo: "10",
         precoCompra: 2.50,
         precoVenda: 7.00,
+        estoque: 0
     }, {
         nome: "Granola Caju 500g",
         codigo: "11",
         precoCompra: 4.50,
         precoVenda: 12.00,
+        estoque: 0
     }, {
         nome: "Granola Caju 1Kg",
         codigo: "12",
         precoCompra: 8.00,
         precoVenda: 15.50,
+        estoque: 0
     }
 ] 
 
