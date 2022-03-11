e8f6bca559f6b3ab701db98d36c0bfd132bfaffe
Walter Soprani Junior <waltersopranijr@gmail.com>
2022-03-11 06:02:26 -0300

    Add function to compare customer invoice with warehouse, returning available items. Table test implemented.

--- index.js
+++ index.js
@@ -13,14 +13,25 @@ function addProdutoNome() {
 
 
 
-function googleLista(array, listaCompras) {
+function customerInvoice(array, invoice) {
 
     let itemPesquisado = [];
 
     for (let i = 0; i < array.length; i++) {
+        console.log(`Item atual no looping: ${array[i].nome}.`)
+        if (array[i].nome == invoice.nome) {
+            console.log(`Item com nome iguao o da lista: ${array[i].nome}.`)
+            console.log(`Item da lista do cliente: ${invoice[i].nome}.`)
+            if(array[i].estoque == invoice[i].qtd) {
 
-        if (array[i].nome == listaCompras) {
-            itemPesquisado.push(array[i]);
+                itemPesquisado.push(array[i]);
+                array[i].estoque = array[i].estoque - invoice[i].qtd
+
+            } else {
+
+                console.log(`A quantidade no estoque não atende o item: ${array[i].nome}. Temos disponibilidade em ${array[i].estoque} ao invés de ${invoice[i].qtd}`)
+
+            }
 
             
         } 
@@ -30,8 +41,14 @@ function googleLista(array, listaCompras) {
 
 
 let resultadoPesquisa = []
-let listaComprasCliente = "Granola Crocante 1Kg"
+let listaComprasCliente = [
+    {
+        nome: "Granola Crocante 1Kg",
+        qtd: 1
+    }
+    ]
 
 
-resultadoPesquisa.push(googleLista(produto, listaComprasCliente))
-console.log('Resultado da busca: ', resultadoPesquisa)
+resultadoPesquisa.push(customerInvoice(produto, listaComprasCliente))
+console.log('Carrinho de compras: ', resultadoPesquisa)
+console.log(resultadoPesquisa)
