55df2b359a9fa19b0bd6156210ecda848dababd2
Walter Soprani Junior <waltersopranijr@gmail.com>
2022-03-11 05:57:32 -0300

    Product list updated.

--- produtos.js
+++ produtos.js
@@ -1,77 +1,96 @@
 const produto = [
     {
         nome: "Granola Tradicional 250g",
-        codigo: "1",
+        codigo: "0001",
         precoCompra: 2.50,
         precoVenda: 7.00,
         estoque: 0
     }, {
         nome: "Granola Tradicional 500g",
-        codigo: "2",
+        codigo: "0002",
         precoCompra: 4.50,
         precoVenda: 12.00,
         estoque: 0
     }, {
         nome: "Granola Tradicional 1Kg",
-        codigo: "3",
+        codigo: "0003",
         precoCompra: 8.00,
         precoVenda: 15.50,
         estoque: 0
     },{
         nome: "Granola Diet 250g",
-        codigo: "4",
+        codigo: "0004",
         precoCompra: 3.50,
         precoVenda: 8.00,
         estoque: 0
     }, {
         nome: "Granola Diet 500g",
-        codigo: "5",
+        codigo: "0005",
         precoCompra: 5.50,
         precoVenda: 13.00,
         estoque: 0
     }, {
         nome: "Granola Diet 1Kg",
-        codigo: "6",
+        codigo: "0006",
         precoCompra: 9.00,
         precoVenda: 16.50,
         estoque: 0
     }, {
         nome: "Granola Cacau 250g",
-        codigo: "7",
+        codigo: "0007",
         precoCompra: 2.50,
         precoVenda: 7.00,
         estoque: 0
     }, {
         nome: "Granola Cacau 500g",
-        codigo: "8",
+        codigo: "0008",
         precoCompra: 4.50,
         precoVenda: 12.00,
         estoque: 0
     }, {
         nome: "Granola Cacau 1Kg",
-        codigo: "9",
+        codigo: "0009",
         precoCompra: 8.00,
         precoVenda: 15.50,
         estoque: 0
     }, {
         nome: "Granola Caju 250g",
-        codigo: "10",
+        codigo: "0010",
         precoCompra: 2.50,
         precoVenda: 7.00,
         estoque: 0
     }, {
         nome: "Granola Caju 500g",
-        codigo: "11",
+        codigo: "0011",
         precoCompra: 4.50,
         precoVenda: 12.00,
         estoque: 0
     }, {
         nome: "Granola Caju 1Kg",
-        codigo: "12",
+        codigo: "0012",
+        precoCompra: 8.00,
+        precoVenda: 15.50,
+        estoque: 0
+    }, {
+        nome: "Granola Crocante 250g",
+        codigo: "0013",
+        precoCompra: 2.50,
+        precoVenda: 7.00,
+        estoque: 0
+    }, {
+        nome: "Granola Crocante 500g",
+        codigo: "0014",
+        precoCompra: 4.50,
+        precoVenda: 12.00,
+        estoque: 0
+    }, {
+        nome: "Granola Crocante 1Kg",
+        codigo: "0015",
         precoCompra: 8.00,
         precoVenda: 15.50,
         estoque: 0
     }
 ] 
 
-console.log(produto)
+
+module.exports = produto
