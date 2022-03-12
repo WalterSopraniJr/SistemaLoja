f28b3817372934c4d59f430dcc1522c8b64b51be
Walter Soprani Junior <waltersopranijr@gmail.com>
2022-03-11 22:19:39 -0300

    Function try out with two new variables to specify customer invoice list details.

--- index.js
+++ index.js
@@ -18,18 +18,21 @@ function customerInvoice(array, invoice) {
     let itemPesquisado = [];
 
     for (let i = 0; i < array.length; i++) {
-        
+
+        let customerItemName = invoice.name
+        let customerItemQtd = invoice.qtd
+
         console.log(`Item atual no looping: ${array[i].nome}.`)
-        if (array[i].nome == invoice) {
+        if (array[i].nome == customerItemName) {
             console.log(`Item com nome iguao o da lista: ${array[i].nome}.`)
-            console.log(`Item da lista do cliente: ${invoice}.`)
-            itemPesquisado.push(array[i]);
-            // if(array[i].estoque == invoice[i].qtd) {
-            //     itemPesquisado.push(array[i]);
-            //     array[i].estoque = array[i].estoque - invoice[i].qtd
-            // } else {
-            //     console.log(`A quantidade no estoque não atende o item: ${array[i].nome}. Temos disponibilidade em ${array[i].estoque} ao invés de ${invoice[i].qtd}`)
-            // }
+            console.log(`Item da lista do cliente: ${customerItemName}.`)
+
+            if(array[i].estoque == invoice[i].qtd) {
+                itemPesquisado.push(array[i]);
+                array[i].estoque = array[i].estoque - invoice[i].qtd
+            } else {
+                console.log(`A quantidade no estoque não atende o item: ${array[i].nome}. Temos disponibilidade em ${array[i].estoque} ao invés de ${invoice[i].qtd}`)
+            }
            
         } 
     }
@@ -38,12 +41,12 @@ function customerInvoice(array, invoice) {
 
 
 let resultadoPesquisa = []
-let listaComprasCliente = [ "Granola Crocante 1Kg"]
-    // {
-    //     nome: "Granola Crocante 1Kg",
-    //     qtd: 1
-    // }
-    // ]
+let listaComprasCliente = [ 
+    {
+        nome: "Granola Crocante 500gr",
+        qtd: 1
+    }
+    ]
 
 
 resultadoPesquisa.push(customerInvoice(produto, listaComprasCliente))
