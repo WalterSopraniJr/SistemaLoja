d005add523734342642c3a8722e3f18150fb7765
Walter Soprani Junior <waltersopranijr@gmail.com>
2022-03-11 22:13:52 -0300

    Table test done and function verified.

--- index.js
+++ index.js
@@ -18,22 +18,19 @@ function customerInvoice(array, invoice) {
     let itemPesquisado = [];
 
     for (let i = 0; i < array.length; i++) {
+        
         console.log(`Item atual no looping: ${array[i].nome}.`)
-        if (array[i].nome == invoice.nome) {
+        if (array[i].nome == invoice) {
             console.log(`Item com nome iguao o da lista: ${array[i].nome}.`)
-            console.log(`Item da lista do cliente: ${invoice[i].nome}.`)
-            if(array[i].estoque == invoice[i].qtd) {
-
-                itemPesquisado.push(array[i]);
-                array[i].estoque = array[i].estoque - invoice[i].qtd
-
-            } else {
-
-                console.log(`A quantidade no estoque não atende o item: ${array[i].nome}. Temos disponibilidade em ${array[i].estoque} ao invés de ${invoice[i].qtd}`)
-
-            }
-
-            
+            console.log(`Item da lista do cliente: ${invoice}.`)
+            itemPesquisado.push(array[i]);
+            // if(array[i].estoque == invoice[i].qtd) {
+            //     itemPesquisado.push(array[i]);
+            //     array[i].estoque = array[i].estoque - invoice[i].qtd
+            // } else {
+            //     console.log(`A quantidade no estoque não atende o item: ${array[i].nome}. Temos disponibilidade em ${array[i].estoque} ao invés de ${invoice[i].qtd}`)
+            // }
+           
         } 
     }
     return itemPesquisado;
@@ -41,12 +38,12 @@ function customerInvoice(array, invoice) {
 
 
 let resultadoPesquisa = []
-let listaComprasCliente = [
-    {
-        nome: "Granola Crocante 1Kg",
-        qtd: 1
-    }
-    ]
+let listaComprasCliente = [ "Granola Crocante 1Kg"]
+    // {
+    //     nome: "Granola Crocante 1Kg",
+    //     qtd: 1
+    // }
+    // ]
 
 
 resultadoPesquisa.push(customerInvoice(produto, listaComprasCliente))
