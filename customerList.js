bb7b545a0521a6fe75fc2ecafec5f4b80aab2c6f
Walter Soprani Junior <waltersopranijr@gmail.com>
2022-03-13 21:39:23 -0300

    Spare customer list in another file.

---
+++ customerList.js
@@ -0,0 +1,10 @@
+//-----------------Lista-de-Compras-do-Cliente---------------------------------
+
+let customerList = [
+    {
+        nome: "Granola Crocante 500g",
+        qtd: 1
+    }
+]
+
+module.exports = customerList
