28c443eb94606a95e44bda6df72ad6ae42a8f211
Walter Soprani Junior <waltersopranijr@gmail.com>
2022-03-13 21:38:24 -0300

    System working.

--- index.js
+++ index.js
@@ -1,59 +1,74 @@
 const produto = require('./produtos')
-const carrinhoCompra = []
+let customerList = require('./customerList')
+let carrinhoCompra = []
+
+console.log("------------------------------------------------------------------------------------------")
 
 
 //------------------Adiciona-Mercadoria-no-Estoque--------------------
 
-function addProduto() {
-    
-    carrinhoCompra.push(carrinhoCompra)
-
-    return carrinhoCompra
-}
-
-
-
-
-
-
-
-
-function customerInvoice(array, invoice) {
-
-    let itemPesquisado = [];
+function addProduto(array, abastecimento) {
+  
+    let itemComparado = [];
 
     for (let i = 0; i < array.length; i++) {
-
-        if (array[i].nome == invoice[0].nome) {
-
-            if(array[i].estoque == invoice[0].qtd) {
-                itemPesquisado.push(array[i]);
-                array[i].estoque = array[i].estoque - invoice[0].qtd
-            } else {
-                console.log(`A quantidade no estoque não atende o item: ${invoice[0].nome}. Temos disponibilidade em ${array[i].estoque} ao invés de ${invoice[0].qtd}`)
-            }
-
-            
+        iList = 0
+        if (array[i].nome == abastecimento[iList].nome) {
+            array[i].estoque = array[i].estoque + abastecimento[iList].qtd
+            iList++
         } 
     }
 
         
-    return itemPesquisado;
+    return itemComparado;
 }
 
-//-----------------Lista-de-Compras-do-Cliente---------------------------------
 
-let customerList = [
+//-----------------Mercadoria-em-Lançamento-no-Estoque----------------
+
+let produtoReposicao = [
     {
         nome: "Granola Crocante 500g",
-        qtd: 1
+        precoCompra: 4.50,
+        qtd: 15
     }
 ]
+
+addProduto(produto, produtoReposicao)
+console.log("Lançando mercadorias no estoque:")
+console.log(`Adicionado ${produtoReposicao[0].qtd} unidade(s) do item ${produtoReposicao[0].nome}`)
+console.log("------------------------------------------------------------------------------------------")
+
+//----------------Adiciona-lista-de-compras-no-Carrinho------------------
+
+function customerInvoice(array, invoice) {
+
+    let itemComparado = [];
+
+    for (let i = 0; i < array.length; i++) {
+        iList = 0
+        if (array[i].nome == invoice[iList = 0].nome) {
+
+            if(array[i].estoque >= invoice[iList = 0].qtd) {
+                itemComparado.push(array[i]);
+                array[i].estoque = array[i].estoque - invoice[iList = 0].qtd
+            } else {
+                console.log(`A quantidade no estoque não atende o item: ${invoice[iList = 0].nome}. Temos disponibilidade em ${array[i].estoque} ao invés de ${invoice[0].qtd}`)
+            }
+            iList++            
+        } 
+    }
+
+        
+    return itemComparado;
+}
+
+
 
 //----------------Resultado-da-Comparação-de-Compras---------------------------
 
 carrinhoCompra.push(customerInvoice(produto, customerList))
-console.log("Carrinho de compras:")
-console.log(`Item: ${carrinhoCompra[0].nome}`)
-console.log(`Quantidade: ${carrinhoCompra[0].qtd}`)
-
+console.log("Carrinho de compras do cliente:")
+console.log(`Item: ${customerList[0].nome}`)
+console.log(`Quantidade: ${customerList[0].qtd}`)
+console.log("------------------------------------------------------------------------------------------")
