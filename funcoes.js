b6241b5a9b18d59ddb0064585271c65d51acf26b
Walter Soprani Junior <waltersopranijr@gmail.com>
2022-03-13 21:41:29 -0300

    This file it's not used.

---
+++ funcoes.js
@@ -0,0 +1,16 @@
+// FILE NOT NEEDED
+
+// function google(array, addProduto) {
+
+//     let itemPesquisado = [];
+
+//     for (let i = 0; i < array.length; i++) {
+
+//         if (array[i].nome == addProduto) {
+//             itemPesquisado.push(array[i]);
+//         } 
+//     }
+//     return itemPesquisado;
+// }
+
+// module.exports = google
